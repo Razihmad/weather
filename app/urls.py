3663738d26e710086d44b5e3ed1d2453a1d5f3dd
Razi Ahmad <mail2razi4u@gmail.com>
2022-01-04 10:47:59 +0530

    temprature with emoji has been done

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from django.urls import path,include
 from app import views
 urlpatterns = [
+    path("",views.home,name='home'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import redirect, render,HttpResponse
 import requests
+from django.contrib import messages
 from app.models import Details
 from django.core.mail import send_mail
 import time
@@ -10,12 +11,27 @@ def home(request):
         name = request.POST.get('name')
         city = request.POST.get('city')
         data = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=531fd30b5908dad912d0c9b28313a7eb").json()
-        temp = data['main']['temp']
-        temp = temp-273
-        temp = int(temp)
-        t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)    
-        send_mail(subject=f"Hi {name}, interested in our services",message=f"{temp} Degree Celcius",
-                  from_email='admin@tutorchamps.com',fail_silently=False,recipient_list=[f"{email}"])    
-        Details(email=email,username=name,city=city,time=current_time).save()
+        if data['cod']==200:
+            temp = data['main']['temp']
+            temp = temp-273
+            temp = int(temp)
+            t = time.localtime()
+            current_time = time.strftime("%H:%M:%S", t)    
+            if temp<=10:
+                
+                send_mail(subject=f"Hi {name}, interested in our services",message=f"{temp}" +'\N{DEGREE SIGN}' + ' '+'\U0001F976',
+                    from_email='admin@tutorchamps.com',fail_silently=False,recipient_list=[f"{email}"])  
+            elif 10<temp<28:
+                send_mail(subject=f"Hi {name}, interested in our services",message=f"{temp}" +'\N{DEGREE SIGN}' + ' '+'\U0001F60C',
+                    from_email='admin@tutorchamps.com',fail_silently=False,recipient_list=[f"{email}"])    
+            else:
+                send_mail(subject=f"Hi {name}, interested in our services",message=f"{temp}" +'\N{DEGREE SIGN}' + ' '+'\U0001F975',
+                    from_email='admin@tutorchamps.com',fail_silently=False,recipient_list=[f"{email}"])  
+                
+            Details(email=email,username=name,city=city,time=current_time).save()
+            messages.success(request,"Temprature of your city has been sent to your mail")
+            return redirect('home')
+        else:
+            messages.warning(request,data['message'])
+            return redirect('home')
     return render(request,'home.html')
